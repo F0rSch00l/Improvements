0b49872d5a51ec8bb0864e63846fea078d61f0d1
F0rSch00l <szamar860@hengersor.hu>
2025-11-03 10:53:03 +0100

    Fájlba írás ❌

---
+++ techs/techs/Improvements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace techs
+{
+    //Év,Előreugrás/Technológia/Esemény,Eseményt/Előreugrást/Technológiát létrehozó személy(ek) neve
+    internal class Improvements
+    {
+        public int year { get; }
+        public string improvement { get; }
+
+        public new List<string> names { get; }
+
+        public Improvements(string line)
+        {
+            string[] currentLine = line.Split('%');
+            year = int.Parse(currentLine[0]);
+            improvement = currentLine[1];
+            names = new List<string>();
+            string[] currentNames = currentLine[2].Split('/');
+            foreach (string s in currentNames)
+            {
+                names.Add(s);
+            }
+            
+        }
+    }
+}
---
+++ techs/techs/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Dynamic;
+
+namespace techs
+{
+    internal class Program
+    {
+        static void readFile(string inputFile, List<Improvements> outputList)
+        {
+            StreamReader sr = new StreamReader(inputFile);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                outputList.Add(new Improvements(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+
+        static int countImprovements(List<Improvements> improvementsList,int fromDate)
+        {
+            List<Improvements> improvementsSince = new List<Improvements>();
+            for(int i = 0; i < improvementsList.Count; i++)
+            {
+                if(improvementsList[i].year > fromDate)
+                    improvementsSince.Add(improvementsList[i]);
+            }
+
+            return improvementsSince.Count;
+        }
+
+        static bool isSthereSomeone(List<Improvements> improvementsList)
+        {
+            List<string> names = new List<string>();
+            for (int i = 0; i < improvementsList.Count; i++)
+            {
+                for(int j = 0; j < improvementsList[i].names.Count; j++)
+                {
+                    if (!names.Contains(improvementsList[i].names[j]))
+                        names.Add(improvementsList[i].names[j]);
+                    else
+                        return true;
+                    
+                }
+                
+
+            }
+            return false;
+        }
+
+        static void showLastImprovement(List<Improvements> improvementsList)
+        {
+            int lastYear = 0;
+            int indexOfLast = 0;
+            for(int i = 0; i < improvementsList.Count; i++)
+            {
+                if (improvementsList[i].year > lastYear)
+                {
+                    lastYear = improvementsList[i].year;
+                    indexOfLast = i;
+                }
+
+            }
+            Console.WriteLine($"Utolsó előugrás: \n Éve: {lastYear} \n Előreugrás: {improvementsList[indexOfLast].improvement} \n Előidező(k) neve: {string.Join(" / ", improvementsList[indexOfLast].names)}");
+        }
+
+        static void Main(string[] args)
+        {
+
+            List<Improvements> improvements = new List<Improvements>();
+            readFile("ProjektHázi.csv", improvements);
+            string yesNo = "Nem";
+            Console.WriteLine($"1830 óta {countImprovements(improvements, 1830)} technológiai előugrást találtam.");
+            Console.WriteLine($"Van(nak)-e olyan ember(ek), akik több technológiai előugrásért is felelősek? - {yesNo = (isSthereSomeone(improvements) ? "Igen" : yesNo)}");
+            showLastImprovement(improvements);
+
+        }
+    }   
+}
